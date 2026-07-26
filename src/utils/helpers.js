e164187775715ac3c58784c08d99181321580ca0
Anam Shaikh <anamshk59@gmail.com>
2026-07-26 19:52:13 +0530

    Updated donor data and helpers to include age, gender, email, state, lastDonationDate

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,9 @@
 // Basic phone number validation
 export const isValidPhone = (phone) => /^[0-9]{10}$/.test(phone);
 
+// Basic email validation
+export const isValidEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
 // Filter donors by blood group and city
 export const filterDonors = (donors, bloodGroup, city) => {
   return donors.filter((d) => {
