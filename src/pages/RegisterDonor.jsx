2bcc478c5d04bee243243c86011d88192f08792d
Muntaha009 <muntahanoorf9@gmail.com>
2026-07-26 19:53:30 +0530

    Improved Register Donor Form

--- src/pages/RegisterDonor.jsx
+++ src/pages/RegisterDonor.jsx
@@ -9,46 +9,48 @@ function RegisterDonor() {
       <form className="register-form">
 
         <label>Full Name</label>
-        <input type="text" placeholder="Enter your full name" />
+        <input type="text" placeholder="Enter your full name" required/>
 
         <label>Age</label>
-        <input type="number" placeholder="Enter your age" />
+        <input type="number" placeholder="Enter your age" required />
 
         <label>Gender</label>
-        <select>
-          <option>Select Gender</option>
-          <option>Male</option>
-          <option>Female</option>
-          <option>Other</option>
+
+        <select required>
+           <option value="">Select Gender</option>
+           <option value="Male">Male</option>
+           <option value="Female">Female</option>
+           <option value="Other">Other</option>
         </select>
 
         <label>Blood Group</label>
-        <select>
-          <option>Select Blood Group</option>
-          <option>A+</option>
-          <option>A-</option>
-          <option>B+</option>
-          <option>B-</option>
-          <option>AB+</option>
-          <option>AB-</option>
-          <option>O+</option>
-          <option>O-</option>
+
+        <select required>
+           <option value="">Select Blood Group</option>
+           <option value="A+">A+</option>
+           <option value="A-">A-</option>
+           <option value="B+">B+</option>
+           <option value="B-">B-</option>
+           <option value="AB+">AB+</option>
+           <option value="AB-">AB-</option>
+           <option value="O+">O+</option>
+           <option value="O-">O-</option>
         </select>
 
         <label>Phone Number</label>
-        <input type="tel" placeholder="Enter phone number" />
+        <input type="tel" placeholder="Enter phone number" required/>
 
         <label>Email</label>
-        <input type="email" placeholder="Enter email" />
+        <input type="email" placeholder="Enter email" required/>
 
         <label>City</label>
-        <input type="text" placeholder="Enter city" />
+        <input type="text" placeholder="Enter city" required/>
 
         <label>State</label>
-        <input type="text" placeholder="Enter state" />
+        <input type="text" placeholder="Enter state" required/>
 
         <label>Last Donation Date</label>
-        <input type="date" />
+        <input type="date" required />
 
         <label>Availability</label>
 
@@ -66,6 +68,9 @@ function RegisterDonor() {
 
         <button type="submit">
           Register Donor
+        </button>
+        <button type="reset" className="reset-btn">
+          Reset Form
         </button>
 
       </form>
